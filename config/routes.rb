331a60ee79258ca3f68a55a7bb098df51fd7e206
Niklas Düttmann <niklas.duettmann033@gmail.com>
2019-08-20 17:11:56 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    resources :planes
-  end
-
-  resources :users, only: [ ] do
-    resources :planes, only: [ :index ]
-  end
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :planes, only: [ :index, :show ]
