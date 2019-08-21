af1aab76ce025f778576017dd77a31c070d7c22e
Donatus Salsali <donatus@salsali.cc>
2019-08-21 11:36:41 +0200

    booking

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :planes, only: [ :index, :show ]
   resources :users, only: [ :index, :show, :destroy ]
-  # resources :reviews, only: [ :new, :create, :destroy]
+
 end
