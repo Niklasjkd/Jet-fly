25b30492c6e455aeec98833a0f4b9653cd088375
Hans-Conradt v. Wedemeyer <hc@vonwedemeyer.com>
2019-08-21 15:16:29 +0200

    implemented search autocomplete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   before_action :set_user, only: [:home]
   def home
-      redirect_to planes_path(search_params) if search_params.present?
+    redirect_to planes_path(search_params) if search_params.present?
   end
 
 private
