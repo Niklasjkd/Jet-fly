3644d04c918a972d7140bc03ec90432f9aa44ee1
Niklas Düttmann <niklas.duettmann033@gmail.com>
2019-08-21 14:58:03 +0200

    pundi error fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   private
 
   def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(^planes$)/
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
   def set_user
