ea682ad813bfeb5bf185a119c97ca07578970844
Angelo Ruperti <arruperti@gmail.com>
2019-08-22 15:21:06 +0200

    added new gem and designed departure date form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,9 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
+import "../plugins/flatpickr"
 
 import { initMapbox } from "../components/init_mapbox";
 initMapbox();
 
 import { initAutocomplete } from "../components/init_autocomplete";
 initAutocomplete();
-
-
