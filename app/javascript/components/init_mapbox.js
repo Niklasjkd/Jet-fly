8f6e03385348b1b5b974e49c7e4213fe3bee61ac
Niklas Düttmann <niklas.duettmann033@gmail.com>
2019-08-23 12:27:11 +0200

    Revert "Revert "Map/feature two markers""

--- app/javascript/components/init_mapbox.js
+++ app/javascript/components/init_mapbox.js
@@ -17,12 +17,42 @@ const addMarkersToMap = (map, markers) => {
     });
 };
 
+const drawLine = (map, markers) => {
+  map.on('load', function () {
+    map.addLayer({
+      "id": "route",
+      "type": "line",
+      "source": {
+        "type": "geojson",
+        "data": {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "LineString",
+            "coordinates": [
+            [markers[0].lng, markers[0].lat],
+            [markers[1].lng, markers[1].lat]
+            ]
+          }
+        }
+      },
+      "layout": {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      "paint": {
+        "line-color": "#888",
+        "line-width": 8
+      }
+    });
+  });
+}
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
+    map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
@@ -31,9 +61,9 @@ const initMapbox = () => {
     addMarkersToMap(map, markers);
 
     fitMapToMarkers(map, markers);
+
+    drawLine(map, markers);
   }
 };
-
-
 
 export { initMapbox };
